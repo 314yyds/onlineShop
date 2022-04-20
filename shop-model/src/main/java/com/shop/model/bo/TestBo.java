40291b0431dccd09ec27f282694de0156578977b
yanghd <616179528@qq.com>
2022-04-21 00:09:55 +0800

    feat:1.增加model模块

---
+++ shop-model/src/main/java/com/shop/model/bo/TestBo.java
@@ -0,0 +1,39 @@
+package com.shop.model.bo;
+
+/**
+ *
+ * 测试bo
+ *
+ * @author yanghd
+ * @version 1.0
+ * @date 2022/4/20 23:46
+ */
+public class TestBo {
+    private Long id;
+    private String name;
+    private String no;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+}
